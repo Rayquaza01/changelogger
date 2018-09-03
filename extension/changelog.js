56b5d83fc779d357da0a0aa1d78e0aa2b52a11ad
Joe Jarvis <joe.jarvis109@gmail.com>
2018-09-02 20:38:30 -0400

    Use document fragments for all elements

--- extension/changelog.js
+++ extension/changelog.js
@@ -2,45 +2,47 @@ const push = document.getElementById("push");
 
 async function main() {
     browser.browserAction.setBadgeText({text: ""});
-    var res = await browser.storage.local.get();
+    const res = await browser.storage.local.get();
     if (res.changelogs.length === 0) {
+        // message if no changelogs are saved yet
         push.innerText = "There are no saved changelogs yet. Changelogs will be added here when extensions are installed or updated."
     }
-    for (var item of res.changelogs) {
-        var container = document.createElement("div");
+    let frag = document.createDocumentFragment();
+    for (let item of res.changelogs) {
+        let container = document.createElement("div");
         container.className = "item";
-        push.append(container);
-        var name = document.createElement("span");
+        frag.append(container);
+        let name = document.createElement("span");
         name.className = "name";
         container.append(name);
-        var icon = document.createElement("img");
+        let icon = document.createElement("img");
         icon.src = item.icon;
         name.append(icon);
-        var link = document.createElement("a");
+        let link = document.createElement("a");
         link.href = item.url;
         link.className = "link";
         link.innerText = item.name;
         name.append(link);
-        var version = document.createElement("span");
+        let version = document.createElement("span");
         version.className = "version";
         version.innerText = item.version;
         name.append(version);
-        var changelog = document.createElement("blockquote");
+        let changelog = document.createElement("blockquote");
         changelog.className = "changelog";
-        var dom = new DOMParser().parseFromString(item.release_notes, "text/html");
-        var links = dom.getElementsByTagName("a");
-        for (var link of links) {
-            var raw = decodeURIComponent(link.href);
+        // use dom parser to remove redirects from links
+        let dom = new DOMParser().parseFromString(item.release_notes, "text/html");
+        let links = dom.getElementsByTagName("a");
+        for (let link of links) {
+            let raw = decodeURIComponent(link.href);
             link.href = raw.match(/https?:\/\/outgoing\.prod\.mozaws\.net\/.*\/(https?:\/\/.*)/)[1];
         }
-        var frag = document.createDocumentFragment();
         while (dom.body.firstChild) {
-            var child = dom.body.removeChild(dom.body.firstChild);
-            frag.append(child);
+            let child = dom.body.removeChild(dom.body.firstChild);
+            changelog.append(child);
         }
-        changelog.append(frag);
         container.append(changelog);
     }
+    push.append(frag);
 }
 
 document.addEventListener("DOMContentLoaded", main);
