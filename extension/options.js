7c6ffcb3bf3fc4d92f517b4b220166fd69d7f24f
kinghat <kinghat@protonmail.com>
2021-02-20 23:57:55 -0600

    set options context to system theme

--- extension/options.js
+++ extension/options.js
@@ -1,18 +1,14 @@
 const badge = document.getElementById("badge");
 const notification = document.getElementById("notification");
 const max = document.getElementById("max");
-const theme = document.getElementById("theme");
 const ignore_no_changelogs = document.getElementById("ignore_no_changelogs");
+const colorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
 async function load() {
     const res = await browser.storage.sync.get("options");
-    if (res.options.theme === "dark") {
-        document.documentElement.dataset.theme = "dark";
-    }
     badge.value = res.options.badge;
     notification.value = res.options.notification;
     max.value = res.options.max;
-    theme.value = res.options.theme;
     ignore_no_changelogs.value = res.options.ignore_no_changelogs;
 }
 
@@ -22,12 +18,18 @@ async function save() {
             badge: JSON.parse(badge.value),
             notification: JSON.parse(notification.value),
             max: parseInt(max.value),
-            theme: theme.value,
             ignore_no_changelogs: JSON.parse(ignore_no_changelogs.value)
         }
     });
-    document.documentElement.dataset.theme = theme.value;
 }
+
+function setColorScheme(e) {
+    e.matches ? document.documentElement.dataset.theme = "dark"
+    : document.documentElement.dataset.theme = "light";
+}
+
+setColorScheme(colorScheme);
 
 document.addEventListener("DOMContentLoaded", load);
 document.addEventListener("change", save);
+colorScheme.addEventListener("change", setColorScheme);
