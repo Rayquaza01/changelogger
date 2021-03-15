1a28add918e57a98eb7737cddaad9cf16da22338
Joe Jarvis <joe.jarvis109@gmail.com>
2021-03-14 20:33:29 -0400

    Changelog and options interface

---
+++ src/Changelog.ts
@@ -0,0 +1,8 @@
+export interface Changelog {
+    id: string;
+    version: string;
+    icon: string;
+    name: string;
+    release_notes: string;
+    url: string;
+}
---
+++ src/Options.ts
@@ -0,0 +1,24 @@
+export interface OptionsInterface {
+    /** Should the icon badge be updated when an extension is updated? */
+    badge: boolean;
+    /** Should a notification be created when an extension is updated */
+    notification: boolean;
+    /** Max number of changelogs to store at once */
+    max: number;
+    /** Should updates with no changelogs be ignored? */
+    ignore_no_changelogs: boolean;
+}
+
+export class Options implements OptionsInterface {
+    badge: boolean;
+    notification: boolean;
+    max: number;
+    ignore_no_changelogs: boolean;
+
+    constructor(opts: Partial<OptionsInterface>) {
+        this.badge = opts.badge ?? true;
+        this.notification = opts.notification ?? true;
+        this.max = opts.max ?? 10;
+        this.ignore_no_changelogs = this.ignore_no_changelogs ?? false;
+    }
+}
